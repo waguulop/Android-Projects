7571b92bc057aa7f0a52b9dbdde1c45fad93018c
waguulop <djguilleyps@gmail.com>
2020-10-29 00:42:23 +0100

    Uso de recyclerView con equipos de futbol, error return arreglado

--- RecyclerViewFutbol/app/src/main/java/com/example/recyclerviewfutbol/RecyclerViewAdaptador.java
+++ RecyclerViewFutbol/app/src/main/java/com/example/recyclerviewfutbol/RecyclerViewAdaptador.java
@@ -39,7 +39,7 @@ public class RecyclerViewAdaptador extends RecyclerView.Adapter<RecyclerViewAdap
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_equipo,parent,false);
         ViewHolder viewHolder = new ViewHolder(view);
-        return null;
+        return viewHolder;
     }
 
     @Override
